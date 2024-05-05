731f6cccc98b0478e0ef51ad48a152013bcdb60d
igorjoxa1118 <igorskorolevs5@gmail.com>
2024-05-05 09:08:44 +0300

    i3-wm dotfiles

--- user/.config/i3/scripts/ExportsVars.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-read -r RICETHEME < "$HOME"/.config/i3/.rice
-export RICETHEME
-PATH="$HOME/.config/i3/scripts:$PATH"
-rice_dir="$HOME/.config/i3/rices/$RICETHEME"
-export XDG_CURRENT_DESKTOP='i3'
