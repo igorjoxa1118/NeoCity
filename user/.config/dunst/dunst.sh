410452f3f892c43a7dab1dd89ed59cdf5b0dcb85
Igors Korolevs <igorskorolevs5@gmail.com>
2025-02-28 17:56:53 +0200

    Dotfiles for i3wm

---
+++ user/.config/dunst/dunst.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+# vars
+# pid="$(pidof dunst)"
+pid="$(ps aux | grep dunstrc | grep -v grep | awk -n '{print $2}')"
+pid2="$(ps aux | grep musnify-mpd | grep -v grep | awk -n '{print $2}')"
+
+function closed {
+	killall dunst
+	killall musnify-mpd
+}
+
+# exec
+if test "$pid"; then
+	if test "$pid2"; then
+		kill -9 "$pid2"
+	fi
+	# killall "dunst"
+	kill -9 "$pid"
+	closed
+fi
+
+function start { 
+	dunst -conf $HOME/.config/dunst/dunstrc &
+	musnify-mpd &
+}
+start
