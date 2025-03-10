d73eb12932552f9b43cdf41e64996576ae0e207d
igorjoxa1118 <igorskorolevs5@gmail.com>
2025-03-10 19:14:41 +0200

    Dotfiles for i3wm

--- user/.config/i3/src/cava.sh
+++ user/.config/i3/src/cava.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-bar="▁▂▃▄▅▆▇█"
-dict="s/;//g"
-
-bar_length=${#bar}
-
-for ((i = 0; i < bar_length; i++)); do
-	dict+=";s/$i/${bar:$i:1}/g"
-done
-
+# Путь к временному конфигурационному файлу CAVA
 config_file="/tmp/bar_cava_config"
+
+# Создание временного конфигурационного файла для CAVA
 cat >"$config_file" <<EOF
 [general]
-bars = 15
+bars = 15  # Количество баров
+sensitivity = 250  # Чувствительность
 
 [input]
 method = pulse
@@ -22,21 +17,54 @@ source = auto
 method = raw
 raw_target = /dev/stdout
 data_format = ascii
-ascii_max_range = 7
+ascii_max_range = 7  # Значение по умолчанию
 
 [color]
-gradient = 1
+gradient = 0  # Отключаем градиент
 
-gradient_color_1 = '#89b4fa'
-gradient_color_2 = '#74c7ec'
-gradient_color_3 = '#94e2d5'
-gradient_color_4 = '#a6e3a1'
-gradient_color_5 = '#f9e2af'
-gradient_color_6 = '#fab387'
-gradient_color_7 = '#f38ba8'
-gradient_color_8 = '#cba6f7'
+[smoothing]
+integral = 70  # Сглаживание баров
+
+[fft]
+mode = high  # Режим FFT
 EOF
 
-pkill -f "cava -p $config_file"
+# Акцентные цвета Catppuccin Mocha
+colors=(
+    "#89b4fa" "#f38ba8" "#fab387" "#a6e3a1" "#94e2d5"
+    "#74c7ec" "#cba6f7" "#f5e0dc" "#89b4fa" "#f38ba8"
+    "#fab387" "#a6e3a1" "#94e2d5" "#74c7ec" "#cba6f7"
+)
 
-cava -p "$config_file" | sed -u "$dict"
+# Запуск CAVA и обработка вывода
+cava -p "$config_file" | while IFS=';' read -r -a bars; do
+    # Строка для вывода в Polybar
+    output=""
+
+    # Проходим по каждому бару
+    for ((i = 0; i < ${#bars[@]}; i++)); do
+        level=${bars[$i]}
+        # Нормализуем уровень до диапазона 0-8 (для символов)
+        normalized_level=$(( (level * 8) / 7 ))
+
+        # Выбираем символ в зависимости от нормализованного уровня
+        case $normalized_level in
+            0) symbol=" ";;
+            1) symbol="▁";;
+            2) symbol="▂";;
+            3) symbol="▃";;
+            4) symbol="▄";;
+            5) symbol="▅";;
+            6) symbol="▆";;
+            7) symbol="▇";;
+            8) symbol="█";;
+            *) symbol=" ";;
+        esac
+
+        # Добавляем цветной символ в строку вывода
+        output+="%{F${colors[$i]}}$symbol%{F-}"
+    done
+
+    # Выводим результат в Polybar
+    echo "$output"
+done
